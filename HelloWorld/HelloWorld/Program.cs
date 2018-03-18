d55382f5f079734dcea85939a5a021c46a82c19b
RC-24 <cullotyross@eircom.net>
2018-03-18 12:27:20 +0000

    Output the string "Hello World!" to the console

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
         }
     }
 }
